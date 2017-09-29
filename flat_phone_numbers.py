f28ed4bb4a7a37b332b8358d7d1076b5c5126981
Mehdi Hasan Aabir <mhasanaabir@gmail.com>
2017-09-29 12:28:30 +0600

    Update flat_phone_numbers.py

--- flat_phone_numbers.py
+++ flat_phone_numbers.py
@@ -1,5 +1,7 @@
 __author__ = 'Aabir'
 
+
+
 def flat_num():
 	flats = ['A','B','C','D','E',]
 	floors = ['1','2','3','4','5','6','7','8']
@@ -11,20 +13,27 @@ def flat_num():
 	return total
 		
 def intercom(fnum):	
-	interlist = []
-	'''will create a list of all numbers'''
-	for number in range(802, 842): 
-		interlist.append(number)
+    interlist = []
+    '''will create a list of all numbers'''
+    for number in range(802, 842): 
+        interlist.append(number)
 	
-	'''will create a dictionary of flat and numbers'''
-	for i in range(40): 
-		a = {fnum[i] : interlist[i]}
-		print (a)
+    '''will create a dictionary of flat and numbers'''
+    combdict = {}
+    for i in range(40): 
+        combdict.update({fnum[i] : interlist[i]})
+    return combdict
+         
+    
 			
 			
 def main():		
-	a = flat_num() 
-	intercom(a)
+    flat_lebels = flat_num() 
+    b = intercom(flat_lebels)
+    
+    query = str(input('Enter flat number: '))
+    print('The intercom number of your chosen flat is: {}'.format(b[query]))
+    
 	
 if __name__ == '__main__': 
 	main()
